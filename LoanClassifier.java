d9f0f86c90447c8cfeeb47c3d39061676130a7ee
Kevin Rowland <krowlan3@jhu.edu>
2015-11-25 12:11:10 -0700

    Mapped label from {0,1} to {-1,1}

--- LoanClassifier.java
+++ LoanClassifier.java
@@ -8,7 +8,9 @@ public class LoanClassifier extends Predictor {
 
 	private int sgd_iterations;
 	private double alpha[];
-	
+	//TODO choose U = C = the margin?
+	private double U;
+
 	public LoanClassifier(int sgd_iterations) {
 	    this.sgd_iterations = sgd_iterations;
 	}
@@ -19,15 +21,17 @@ public class LoanClassifier extends Predictor {
 		// There is an alpha for each sample.
 		alpha = new double[instances.size()];
 		
-		// Iterate over each sample sgd_ierations times
+		// Iterate over each sample sgd_iterations times
+		// This is the paper's "outer iteration"
 		for(int i = 0; i < sgd_iterations; i++) {
 			
 			// Compute the W-Vector.
+			// I think we can start with a 0 vector here
 			double wValues[] = new double[instances.size()];
 			int j = 0;
 			for(Instance instance : instances) {
 				// Fetch the sample's label.
-				double label = Integer.parseInt(instance.getLabel().toString());
+				double label = Integer.parseInt(instance.getLabel().toString())*2 - 1;
 				wValues[j] = label * alpha[j] * 1;
 				j++;
 			}
@@ -38,13 +42,14 @@ public class LoanClassifier extends Predictor {
 				// Fetch the sample's label.
 				double label = Integer.parseInt(instance.getLabel().toString());
 				
-				// Compute the G term - we are using L1 loss function, so D_ii is 0.
+				// Compute the G term (gradient) - we are using L1 loss function, so D_ii is 0.
 				double GTerm = 0.0;
 				for(int feature : instance._feature_vector.sparseVector.keySet()) {
 					GTerm += label * wValues[j] * instance._feature_vector.get(feature);
 				}
 				GTerm--;
 				
+				// Now use the gradient to optimize a^{k,i+1}_i
 				j++;
 			}
 		}
