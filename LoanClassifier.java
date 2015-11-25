b0652709942d2f79ac5b525e7fd5a226f9501b5a
Kevin Rowland <krowlan3@jhu.edu>
2015-11-25 12:50:10 -0700

    Moved weights vector initialization out of loop

--- LoanClassifier.java
+++ LoanClassifier.java
@@ -8,6 +8,7 @@ public class LoanClassifier extends Predictor {
 
 	private int sgd_iterations;
 	private double alpha[];
+	private double weights[];
 	//TODO choose U = C = the margin?
 	private double U;
 
@@ -20,19 +21,19 @@ public class LoanClassifier extends Predictor {
 	    
 		// There is an alpha for each sample.
 		alpha = new double[instances.size()];
-		
+		// And a weights vector for easy computation of the alphas
+		weights = new double[instances.size()];
+
 		// Iterate over each sample sgd_iterations times
 		// This is the paper's "outer iteration"
 		for(int i = 0; i < sgd_iterations; i++) {
 			
-			// Compute the W-Vector.
-			// I think we can start with a 0 vector here
-			double wValues[] = new double[instances.size()];
+			// Compute the weights vector.
 			int j = 0;
 			for(Instance instance : instances) {
 				// Fetch the sample's label.
 				double label = Integer.parseInt(instance.getLabel().toString())*2 - 1;
-				wValues[j] = label * alpha[j] * 1;
+				weights[j] = label * alpha[j] * 1;
 				j++;
 			}
 			
@@ -45,7 +46,7 @@ public class LoanClassifier extends Predictor {
 				// Compute the G term (gradient) - we are using L1 loss function, so D_ii is 0.
 				double GTerm = 0.0;
 				for(int feature : instance._feature_vector.sparseVector.keySet()) {
-					GTerm += label * wValues[j] * instance._feature_vector.get(feature);
+					GTerm += label * weights[j] * instance._feature_vector.get(feature);
 				}
 				GTerm--;
 				
